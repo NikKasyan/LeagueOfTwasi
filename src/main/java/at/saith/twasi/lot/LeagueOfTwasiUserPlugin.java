d076c5761639613f6c5c49686cb57b6322db84ee
NikKasyan <niki.kasyan@gmail.com>
2019-01-21 09:42:21 +0100

    + Removed Tests

--- src/main/java/at/saith/twasi/lot/LeagueOfTwasiUserPlugin.java
+++ src/main/java/at/saith/twasi/lot/LeagueOfTwasiUserPlugin.java
@@ -1,10 +1,8 @@
 package at.saith.twasi.lot;
 
 import at.saith.twasi.lot.variable.RankVariable;
-import net.twasi.core.models.Message.TwasiMessage;
 import net.twasi.core.plugin.api.TwasiUserPlugin;
 import net.twasi.core.plugin.api.TwasiVariable;
-import net.twasi.core.plugin.api.events.TwasiMessageEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,11 +16,4 @@ public class LeagueOfTwasiUserPlugin extends TwasiUserPlugin {
         return list;
     }
 
-    @Override
-    public void onMessage(TwasiMessageEvent e) {
-        TwasiMessage message = e.getMessage();
-        if (message.getMessage().equalsIgnoreCase("rank")) {
-            message.reply("$rank(Saith1998)");
-        }
-    }
 }
